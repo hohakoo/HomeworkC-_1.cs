be406114c4703a327291bb3ee9dab64d80603a75
hohakoo <112114231+hohakoo@users.noreply.github.com>
2022-10-12 19:48:11 +0300

    Add files via upload

---
+++ HomeworkC#_2.cs
@@ -0,0 +1,10 @@
+﻿void circumference_and_square_of_circle(int radius)
+{
+    double PI = 3.14;
+    Console.WriteLine($"Circle radius = {radius}, diameter = {radius*2}:");
+    double circumference = 2 * PI * radius;
+    Console.WriteLine($"C = {circumference}");
+    double square = PI * (Math.Pow(radius, 2));
+    Console.WriteLine($"S = {square}");
+}
+circumference_and_square_of_circle(3);
