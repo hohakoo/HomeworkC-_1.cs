528328e8c1a1b0a4ee1220e0478cd048d834da61
hohakoo <112114231+hohakoo@users.noreply.github.com>
2022-10-12 19:46:13 +0300

    Add files via upload

---
+++ HomeworkC#_1.cs
@@ -0,0 +1,9 @@
+﻿void perimeter_and_square(int length, int width)
+{
+    Console.WriteLine($"Rectangle length = {length}, width = {width}:");
+    int perimeter = length * 2 + width * 2;
+    Console.WriteLine($"P = {perimeter}");
+    int square = length * width;
+    Console.WriteLine($"S = {square}");
+}
+perimeter_and_square(1, 2);
