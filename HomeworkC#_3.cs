88998cc7b1a11ff0c87d783479598f7a8fd92311
hohakoo <112114231+hohakoo@users.noreply.github.com>
2022-10-19 21:32:21 +0300

    Add files via upload

---
+++ HomeworkC#_3.cs
@@ -0,0 +1,14 @@
+﻿void ShowTriangle()
+    {
+        Console.Write("Height: ");
+        int height = int.Parse(Console.ReadLine());
+        for (int i = 0; i < height; i++)
+        {
+            for (int j = 0; j <= i; j++)
+            {
+                Console.Write("#");
+            }
+            Console.WriteLine();
+        }
+    }
+ShowTriangle();
